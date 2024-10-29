e140efdca278ca53e0bcb8b5331241ef147db55f
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-28 23:43:32 -0500

    chore: setup project

---
+++ src/main/java/com/yarasoftware/workshopngine/platform/WorkshopNGinePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yarasoftware.workshopngine.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopNGinePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopNGinePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yarasoftware/workshopngine/platform/WorkshopNGinePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yarasoftware.workshopngine.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopNGinePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
