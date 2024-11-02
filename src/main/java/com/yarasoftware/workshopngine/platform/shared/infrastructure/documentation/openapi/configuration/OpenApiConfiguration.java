60105cdfba75b13ba6c9c2c59640268b101d45ad
Fabricio Lopez U202215695 <u202215695@upc.edu.pe>
2024-11-02 03:12:26 -0500

    feat:add and complete shared

--- src/main/java/com/yarasoftware/workshopngine/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,21 +1,44 @@
 package com.yarasoftware.workshopngine.platform.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class OpenApiConfiguration {
     @Bean
-    public OpenAPI workshopngineOpenApi() {
-        return new OpenAPI()
+    public OpenAPI learningPlatformOpenApi() {
+// General configuration
+        var openApi = new OpenAPI();
+        openApi
                 .info(new Info()
-                        .title("Workshop N-GINE API")
-                        .description("API documentation for the Workshop N-GINE application, a platform for automotive workshop management.")
+                        .title("AgroSupport API")
+                        .description("AgroSupport RESTful API documentation.")
                         .version("v1.0.0")
                         .license(new License().name("Apache 2.0")
-                                .url("https://springdoc.org")));
+                                .url("https://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("AgroSupport API Wiki Documentation")
+                        .url("https://github.com/AgroSupport-UPC/Backend/tree/main#readme"));
+        // Add security scheme
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+        return openApi;
     }
 }
+
+
--- src/main/java/com/yarasoftware/workshopngine/platform/shared/infrastructure/persistence/jpa/strategy/SnakeCaseWithPluralizedTablePhysicalNamingStrategy.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/shared/infrastructure/persistence/jpa/strategy/SnakeCaseWithPluralizedTablePhysicalNamingStrategy.java
@@ -5,33 +5,32 @@ import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
 import static io.github.encryptorcode.pluralize.Pluralize.pluralize;
-
-public class SnakeCasePhysicalNamingStrategy implements PhysicalNamingStrategy {
+public class SnakeCaseWithPluralizedTablePhysicalNamingStrategy implements PhysicalNamingStrategy {
     @Override
-    public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+    public Identifier toPhysicalCatalogName(Identifier identifier,
+                                            JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
-
     @Override
-    public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+    public Identifier toPhysicalSchemaName(Identifier identifier,
+                                           JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
-
     @Override
-    public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+    public Identifier toPhysicalTableName(Identifier identifier,
+                                          JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(this.toPlural(identifier));
     }
-
     @Override
-    public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+    public Identifier toPhysicalSequenceName(Identifier identifier,
+                                             JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
-
     @Override
-    public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
+    public Identifier toPhysicalColumnName(Identifier identifier,
+                                           JdbcEnvironment jdbcEnvironment) {
         return this.toSnakeCase(identifier);
     }
-
     private Identifier toSnakeCase(final Identifier identifier) {
         if (identifier == null) {
             return null;
@@ -43,7 +42,6 @@ public class SnakeCasePhysicalNamingStrategy implements PhysicalNamingStrategy {
                 .toLowerCase();
         return Identifier.toIdentifier(newName);
     }
-
     private Identifier toPlural(final Identifier identifier) {
         final String newName = pluralize(identifier.getText());
         return Identifier.toIdentifier(newName);
