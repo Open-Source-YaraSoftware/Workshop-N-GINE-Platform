11890cdd496b3677a46459b603a199c16f86f2cd
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-02 00:23:34 -0500

    feat(IAM): implement RoleRepository and UserRepository JPA repositories
    - Created `RoleRepository` to check role existence by name.
    - Implemented `UserRepository` for user lookup by username and filtering by role and workshop ID.

---
+++ src/main/java/com/yarasoftware/workshopngine/platform/iam/infrastructure/persistence/jpa/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.yarasoftware.workshopngine.platform.iam.infrastructure.persistence.jpa.repositories;
+
+import com.yarasoftware.workshopngine.platform.iam.domain.model.aggregates.User;
+import com.yarasoftware.workshopngine.platform.iam.domain.model.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    List<User> findByRoleAndWorkshopId(Role role, Long workshopId);
+}
