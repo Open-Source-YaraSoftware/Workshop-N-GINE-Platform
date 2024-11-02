3ecac146e74dd64dcf10b7f295a3eed9655ebb67
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-02 00:09:24 -0500

    feat(IAM): add enums for AccountStatuses and Roles
    - Created AccountStatuses enum with values: ACTIVE, INACTIVE, LOCKED, and DELETED.
    - Created Roles enum with values: MECHANIC, WORKSHOP_OWNER, and CLIENT.

---
+++ src/main/java/com/yarasoftware/workshopngine/platform/iam/domain/model/valueobjects/Roles.java
@@ -0,0 +1,10 @@
+package com.yarasoftware.workshopngine.platform.iam.domain.model.valueobjects;
+
+/**
+ * Enum for roles
+ */
+public enum Roles {
+    MECHANIC,
+    WORKSHOP_OWNER,
+    CLIENT
+}
