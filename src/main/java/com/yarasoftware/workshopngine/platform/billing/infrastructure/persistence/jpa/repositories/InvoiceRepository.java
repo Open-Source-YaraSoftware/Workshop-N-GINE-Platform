ee0c6b75a2028c839b190782794689855741ede8
Johan Quiñones <91556525+johaanq@users.noreply.github.com>
2024-11-16 12:55:32 -0500

    feat(billing):  add commandservices, queryservices and repository for invoice

--- src/main/java/com/yarasoftware/workshopngine/platform/billing/infrastructure/persistence/jpa/repositories/InvoiceRepository.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/billing/infrastructure/persistence/jpa/repositories/InvoiceRepository.java
@@ -1,4 +1,12 @@
 package com.yarasoftware.workshopngine.platform.billing.infrastructure.persistence.jpa.repositories;
 
-public class InvoiceRepository {
+import com.yarasoftware.workshopngine.platform.billing.domain.model.aggregates.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    List<Invoice> findBySubscriptionId(Long subscriptionId);
 }
