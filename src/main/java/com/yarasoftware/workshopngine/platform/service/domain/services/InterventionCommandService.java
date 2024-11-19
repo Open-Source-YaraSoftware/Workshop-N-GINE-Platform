75dc00eab60c0bb5d81abe4174d49eedfa4b4418
elmer-riva <elmeraugustoriva@gmail.com>
2024-11-19 05:06:01 -0500

    feat(interventions): add records handle for interfaces services

--- src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionCommandService.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionCommandService.java
@@ -1,8 +1,8 @@
 package com.yarasoftware.workshopngine.platform.service.domain.services;
 
 import com.yarasoftware.workshopngine.platform.service.domain.model.aggregates.Intervention;
-import com.yarasoftware.workshopngine.platform.service.domain.model.commands.CreateInterventionCommand;
-import com.yarasoftware.workshopngine.platform.service.domain.model.commands.UpdateInterventionCommand;
+import com.yarasoftware.workshopngine.platform.service.domain.model.commands.*;
+import com.yarasoftware.workshopngine.platform.service.domain.model.entities.Task;
 
 import java.util.Optional;
 
@@ -10,4 +10,16 @@ public interface InterventionCommandService {
     Optional<Intervention> handle(CreateInterventionCommand command);
 
     Optional<Intervention> handle(Long interventionId, UpdateInterventionCommand query);
+
+    Optional<Task> handle(Long interventionId, CreateTaskCommand command);
+
+    Optional<Task> handle(Long interventionId, Long taskId, UpdateTaskCommand command);
+
+    boolean handle(Long interventionId, DeleteTaskCommand command);
+
+    Optional<Long> handle(InProgressInterventionCommand command);
+
+    Optional<Long> handle(CompleteInterventionCommand command);
+
+    Optional<Long> handle(CancelInterventionCommand command);
 }
--- src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionQueryService.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionQueryService.java
@@ -1,7 +1,10 @@
 package com.yarasoftware.workshopngine.platform.service.domain.services;
 
 import com.yarasoftware.workshopngine.platform.service.domain.model.aggregates.Intervention;
+import com.yarasoftware.workshopngine.platform.service.domain.model.entities.Task;
 import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetAllInterventionsByVehicleIdQuery;
+import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetAllTasksByInterventionIdAndAssistantIdQuery;
+import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetAllTasksByInterventionIdQuery;
 import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetInterventionByIdQuery;
 
 import java.util.List;
@@ -11,4 +14,8 @@ public interface InterventionQueryService {
     Optional<Intervention> handle(GetInterventionByIdQuery query);
 
     List<Intervention> handle(GetAllInterventionsByVehicleIdQuery query);
+
+    List<Task> handle(Long interventionId, GetAllTasksByInterventionIdQuery query);
+
+    List<Task> handle(Long interventionId, GetAllTasksByInterventionIdAndAssistantIdQuery query);
 }
