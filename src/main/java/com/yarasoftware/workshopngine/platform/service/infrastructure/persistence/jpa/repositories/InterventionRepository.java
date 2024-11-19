8b5aff9da91d49c700849fb1367497014676e9e7
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-19 01:50:53 -0500

    feat(interventions-repository): create new methods for validate existence and get all

---
+++ src/main/java/com/yarasoftware/workshopngine/platform/service/infrastructure/persistence/jpa/repositories/InterventionRepository.java
@@ -0,0 +1,17 @@
+package com.yarasoftware.workshopngine.platform.service.infrastructure.persistence.jpa.repositories;
+
+import com.yarasoftware.workshopngine.platform.service.domain.model.aggregates.Intervention;
+import com.yarasoftware.workshopngine.platform.service.domain.model.valueobjects.InterventionStatuses;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface InterventionRepository extends JpaRepository<Intervention, Long> {
+    boolean existsByWorkshopIdAndVehicleIdAndStatusIs(Long workshopId, Long vehicleId, InterventionStatuses status);
+    boolean existsByMechanicLeaderIdAndScheduledAtIsBetween(Long mechanicLeaderId, LocalDateTime start, LocalDateTime end);
+    boolean existsByWorkshopIdAndScheduledAtIsBetween(Long workshopId, LocalDateTime start, LocalDateTime end);
+    List<Intervention> findAllByVehicleId(Long vehicleId);
+}
