fb9b9a285b0c60642550e725d38b9a0f18af3429
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-19 06:32:17 -0500

    feat: update interfaces for command and query services for managing checkpoint

--- src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionCommandService.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionCommandService.java
@@ -2,6 +2,7 @@ package com.yarasoftware.workshopngine.platform.service.domain.services;
 
 import com.yarasoftware.workshopngine.platform.service.domain.model.aggregates.Intervention;
 import com.yarasoftware.workshopngine.platform.service.domain.model.commands.*;
+import com.yarasoftware.workshopngine.platform.service.domain.model.entities.Checkpoint;
 import com.yarasoftware.workshopngine.platform.service.domain.model.entities.Task;
 
 import java.util.Optional;
@@ -17,9 +18,19 @@ public interface InterventionCommandService {
 
     boolean handle(Long interventionId, DeleteTaskCommand command);
 
+    Optional<Checkpoint> handle(Long interventionId, Long taskId, CreateCheckpointCommand command);
+
+    Optional<Checkpoint> handle(Long interventionId, Long taskId, Long checkpointId, UpdateCheckpointCommand command);
+
+    boolean handle(Long interventionId, Long taskId, DeleteCheckpointCommand command);
+
     Optional<Long> handle(InProgressInterventionCommand command);
 
     Optional<Long> handle(CompleteInterventionCommand command);
 
     Optional<Long> handle(CancelInterventionCommand command);
+
+    Optional<Long> handle(Long interventionId, InProgressTaskCommand command);
+
+    Optional<Long> handle(Long interventionId, CompleteTaskCommand command);
 }
--- src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionQueryService.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/service/domain/services/InterventionQueryService.java
@@ -1,11 +1,9 @@
 package com.yarasoftware.workshopngine.platform.service.domain.services;
 
 import com.yarasoftware.workshopngine.platform.service.domain.model.aggregates.Intervention;
+import com.yarasoftware.workshopngine.platform.service.domain.model.entities.Checkpoint;
 import com.yarasoftware.workshopngine.platform.service.domain.model.entities.Task;
-import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetAllInterventionsByVehicleIdQuery;
-import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetAllTasksByInterventionIdAndAssistantIdQuery;
-import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetAllTasksByInterventionIdQuery;
-import com.yarasoftware.workshopngine.platform.service.domain.model.queries.GetInterventionByIdQuery;
+import com.yarasoftware.workshopngine.platform.service.domain.model.queries.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,4 +16,6 @@ public interface InterventionQueryService {
     List<Task> handle(Long interventionId, GetAllTasksByInterventionIdQuery query);
 
     List<Task> handle(Long interventionId, GetAllTasksByInterventionIdAndAssistantIdQuery query);
+
+    List<Checkpoint> handle(GetAllCheckpointsByTaskIdAndInterventionIdQuery query);
 }
