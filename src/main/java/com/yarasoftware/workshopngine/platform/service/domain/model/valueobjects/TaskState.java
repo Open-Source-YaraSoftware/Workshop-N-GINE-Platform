3b933261b3bcb610f21381e7e954fb6f0fd68869
elmer-riva <elmeraugustoriva@gmail.com>
2024-11-19 05:02:46 -0500

    feat: add enum for status of a task

--- src/main/java/com/yarasoftware/workshopngine/platform/service/domain/model/valueobjects/TaskState.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/service/domain/model/valueobjects/TaskState.java
@@ -4,11 +4,11 @@ package com.yarasoftware.workshopngine.platform.service.domain.model.valueobject
  * Represents the state of a task.
  * <p>
  * A task can be in one of the following states:
- * <div><span>{@link #PENDING}</span>, <span>{@link #CANCELLED}</span>, <span>{@link #DONE}</span></div>
+ * <div><span>{@link #PENDING}</span>, <span>{@link #IN_PROGRESS}</span>, <span>{@link #DONE}</span></div>
  * <p>
  */
 public enum TaskState {
     PENDING,
-    CANCELLED,
+    IN_PROGRESS,
     DONE
 }
