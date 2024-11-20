f897a526d22be82828ea4cb258c68e8042f1ee03
Eric <erich56000@gmail.com>
2024-11-20 02:44:22 -0500

    refactor(swagger): update open api and cors configuration so that the swagger makes https requests

--- src/main/java/com/yarasoftware/workshopngine/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -3,8 +3,11 @@ package com.yarasoftware.workshopngine.platform.shared.infrastructure.documentat
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
@@ -16,6 +19,9 @@ public class OpenApiConfiguration {
                         .description("API documentation for the Workshop N-GINE application, a platform for automotive workshop management.")
                         .version("v1.0.0")
                         .license(new License().name("Apache 2.0")
-                                .url("https://springdoc.org")));
+                                .url("https://springdoc.org")))
+                .servers(List.of(
+                        new Server().url("https://workshop-n-gine-platform-production.up.railway.app")
+                ));
     }
 }
