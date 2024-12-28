1b87eb50a7e989504e115c2eaa03ed4af0aa9310
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-12-28 04:19:47 -0500

    refactor(file): change getName for getOriginalFilename to get the original name not only file

--- src/main/java/com/yarasoftware/workshopngine/platform/file/interfaces/rest/transform/CreateFileCommandFromResourceAssembler.java
+++ src/main/java/com/yarasoftware/workshopngine/platform/file/interfaces/rest/transform/CreateFileCommandFromResourceAssembler.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class CreateFileCommandFromResourceAssembler {
     public static CreateFileCommand toCommandFromResource(CreateFileResource resource) throws IOException {
         return new CreateFileCommand(
-                resource.file().getName(),
+                resource.file().getOriginalFilename(),
                 resource.file().getContentType(),
                 resource.file().getSize(),
                 resource.file().getBytes()
